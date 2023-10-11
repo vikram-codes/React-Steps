bdd37dd57a39abc6c5c2a56842881ef62bce473a
Vikram Saini <vikramjit.personal@gmail.com>
2023-10-11 15:08:21 +0530

    Added open and close view functionality

--- steps/src/App.jsx
+++ steps/src/App.jsx
@@ -5,6 +5,10 @@ import "./App.css";
 
 function App() {
   let [step, setStep] = useState(1);
+  let [isOpen, setIsOpen] = useState(true);
+
+  const close = "Close";
+  const open = "Open";
 
   const stepIncrease = function () {
     if (step <= 2) {
@@ -21,31 +25,41 @@ function App() {
   };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        // <button>&times;</button>
+        // #a020f0
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={stepDecrease}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={stepIncrease}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={stepDecrease}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={stepIncrease}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
